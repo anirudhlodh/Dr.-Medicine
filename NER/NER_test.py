ef462fb632a31cd47769f1ecec2f58bddc5d8865
Anirudh Lodh <anirudhlodh@pop-os.localdomain>
2021-10-02 21:27:34 +0530

    Everything fine

--- NER/NER_test.py
+++ NER/NER_test.py
@@ -1,7 +1,7 @@
 # test the trained model
 import spacy
 nlp = spacy.load('NER/NER_dr_medicine')
-test_text = "James went to buy Aspirin last year 2020"
+test_text = "10010 TABLETS aspirin TABLETS BP 500 mg Dolo MICRO"
 doc = nlp(test_text)
 print("Entities in '%s'" % test_text)
 for ent in doc.ents:
