c9fed6f0d70f2e4a66bd0d8214036acd462074c8
Anirudh Lodh <anirudhlodh@pop-os.localdomain>
2021-10-03 16:33:34 +0530

    Backend complete

--- Scraping script/test.py
+++ Scraping script/test.py
@@ -3,12 +3,12 @@ from selenium.webdriver.common.keys import Keys
 from time import sleep
 import os
 from selenium.webdriver.common.by import By
-import unicodedata
+# import unicodedata
 
 class MyBot:
     def __init__(self):
         
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
+        user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36"
         
         self.options = webdriver.ChromeOptions()
         self.options.headless = True
@@ -24,9 +24,9 @@ class MyBot:
         self.options.add_argument('--disable-dev-shm-usage')
         self.options.add_argument('--no-sandbox')
 
-        self.driver = webdriver.Chrome(executable_path="chromedriver.exe", options=self.options)
+        self.driver = webdriver.Chrome(executable_path="Scraping script/chromedriver", options=self.options)
 
-        drug_query_name = "epinephrine"
+        drug_query_name = "adrenaline"
 
         self.driver.get("https://go.drugbank.com/")
         self.driver.find_element(By.XPATH, '/html/body/main/div/div[1]/div[1]/div[2]/form/div[1]/div/input').send_keys(drug_query_name) # entering text in search bar       
---
+++ Spelling_correction/spellchecker.py
@@ -0,0 +1,7 @@
+from gingerit.gingerit import GingerIt
+
+text = 'paracetamoi'
+
+parser = GingerIt()
+ct = parser.parse(text)
+print(ct) 
