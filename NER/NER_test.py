08a1af87a8b0e82d491a6bd682cba53a546ba24d
Utkarsh Saxena <utkarsh@pop-os.localdomain>
2021-10-04 10:33:17 +0530

    Final NER

--- NER/NER_test.py
+++ NER/NER_test.py
@@ -1,7 +1,8 @@
+# code by utkarsh saxena
 # test the trained model
 import spacy
 nlp = spacy.load('NER/NER_dr_medicine_new.spacy')
-test_text = "paracetamol"
+test_text = "i bought Paracetamol"
 doc = nlp(test_text)
 print("Entities in '%s'" % test_text)
 for ent in doc.ents:
