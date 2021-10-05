438d75d79a72470624d88a5d0772660cdee4f444
Anirudh Lodh <anirudhlodh@pop-os.localdomain>
2021-10-05 21:49:06 +0530

    pull push conflict resolved

--- NER/NER_test.py
+++ NER/NER_test.py
@@ -2,11 +2,7 @@
 # test the trained model
 import spacy
 nlp = spacy.load('NER/NER_dr_medicine_new.spacy')
-<<<<<<< HEAD
 test_text = "1015 Capsules R> Atorvastatin And Aspirin Capsules Ecosprin-AV 75 sqiR4T-3c9ox lorvastatinAnd Aspirin Capsules TJIN"
-=======
-test_text = "i bought Paracetamol"
->>>>>>> 08a1af87a8b0e82d491a6bd682cba53a546ba24d
 doc = nlp(test_text)
 print("Entities in '%s'" % test_text)
 for ent in doc.ents:
